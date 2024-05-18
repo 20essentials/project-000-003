6530be0276fbbb3fbd4d0e88f12eaa377760bb93
20essentials <s0l0estudi4r@gmail.com>
2024-05-18 19:39:46 +0200

    v0.0.1

---
+++ README.md
@@ -0,0 +1,3 @@
+![n1](preview/N1.png)
+![n2](preview/N2.png)
+![n3](preview/N3.png)
---
+++ script.js
@@ -0,0 +1,7 @@
+console.groupCollapsed("Reference: ");
+console.log("img-2 ->", "https://wallhaven.cc/w/396lmd");
+console.log("Loader 1->", "https://uiverse.io/JaydipPrajapati1910/smart-falcon-33"); 
+console.log("Loader 2->", "https://uiverse.io/xXJollyHAKERXx/lucky-falcon-75"); 
+console.log("Loader 3->", "https://uiverse.io/mrhyddenn/giant-lion-34"); 
+console.log("Loader 4->", "https://uiverse.io/Nawsome/wet-mayfly-23"); 
+console.groupEnd();
